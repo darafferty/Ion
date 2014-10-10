a299e16e3564f1fdbc38588503d8cf16662d19ad
David Rafferty <drafferty@hs.uni-hamburg.de>
2014-10-10 09:43:26 +0200

    Debug

--- ion_peel.py
+++ ion_peel.py
@@ -1224,7 +1224,7 @@ if __name__=='__main__':
 
         # Set up output directories and initialize logging
         outdir = options.outdir
-        print(outdir)
+        0/0
         if not os.path.isdir(outdir):
             os.mkdir(outdir)
             logfilename = outdir + '/' + outfile + '.log'
